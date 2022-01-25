9c230c3b62ca9754369141f18851b1c5fb0a2535
Santiago Avila <sanavila2002@gmail.com>
2022-01-25 17:19:20 -0500

    Animations and sounds added. Animations were implement in cards div Sounds were implement when the user click on a card and when the winner is declared Changes in the information div to show the logs in one space and avoid create 5 of them. Animation will be implement in this section

--- js.js
+++ js.js
@@ -1,15 +1,16 @@
 let computerPoints = 0;
 let userPoints = 0;
+let counter = 0;
 
 // Dynamic Game
 
 const userScore = document.querySelector('.userScore_1')
-const winnerSelect = document.querySelector('.winner_1')
+const roundAndWinner = document.querySelector('.roundAndWinner-p')
 const computerScore = document.querySelector('.computerScore_1')
 const rock = document.querySelector('.rock')
 const paper = document.querySelector('.paper')
 const scissors = document.querySelector('.scissors')
-const information = document.querySelector('.information')
+const logsGame = document.querySelector('.info')
 
 function computerPlay() {
     let randomNumber = Math.floor(Math.random() * 3) + 1
@@ -29,24 +30,26 @@ function playerSelection() {
     rock.addEventListener('click', () => {
         userSelection = "Rock"
         messageUser = "Your selection was Rock!"
+        counter = counter + 1
         game();
     });
 
-    paper.addEventListener("click", () => {
+    paper.addEventListener('click', () => {
         userSelection = "Paper"
         messageUser = "Your selection was Paper!"
-       game();
+        counter = counter + 1
+        game();
     })
 
     scissors.addEventListener('click', () => {
         userSelection = "Scissors"
         messageUser = "Your selection was Scissors!"
+        counter = counter + 1
         game();
     })
-
 }
 
-function playRound(computerPlay, playerSelection) {
+function playRound() {
     if (objectCompu === userSelection) {
         messageRound = "Tie!";
     } else if (userSelection == "Scissors") {
@@ -77,24 +80,20 @@ function playRound(computerPlay, playerSelection) {
 }
 
 function logs() {
-    const logs = document.createElement('div')
-    logs.classList.add('info')
-    information.appendChild(logs)
-
-    const logUser = document.createElement('p')
+    const logUser = document.querySelector('.userLog')
     logUser.classList.add('infoLogs')
     logUser.textContent = messageUser
-    logs.appendChild(logUser);
+    logsGame.appendChild(logUser);
 
-    const logComputer = document.createElement('p')
+    const logComputer = document.querySelector('.computerLog')
     logComputer.classList.add('infoLogs')
     logComputer.textContent = messageComputer
-    logs.appendChild(logComputer);
+    logsGame.appendChild(logComputer);
 
-    const logRound = document.createElement('p')
+    const logRound = document.querySelector('.roundLog')
     logRound.classList.add('infoLogs-1')
     logRound.textContent = messageRound
-    logs.appendChild(logRound);
+    logsGame.appendChild(logRound);
 }
 
 function reportScore() {
@@ -102,25 +101,65 @@ function reportScore() {
     computerScore.textContent = `Computer Score: ${computerPoints}`
 }
 
-function winner(){
-    if (computerPoints > userPoints){
-        winnerSelect.textContent = "Computer is the winner!"
+function round() {
+    roundAndWinner.textContent = `Round: ${counter} `
+}
+
+function winner() {
+    if (computerPoints > userPoints) {
+        roundAndWinner.textContent = "Computer is the winner!"
     } else if (computerPoints < userPoints) {
-        winnerSelect.textContent = "You are the winner"
+        roundAndWinner.textContent = "You are the winner"
     } else {
-        winnerSelect.textContent = "Is a tie! Try again"
+        roundAndWinner.textContent = "Is a tie! Try again"
     }
 }
 
+function soundEffect() {
+    //Click on cards
+    const audioEffect = document.querySelector("audio");
+    rock.addEventListener('click', () => {
+        audioEffect.setAttribute("src", "sounds/button-click.mp3")
+        audioEffect.play();
+    });
+    paper.addEventListener('click', () => {
+        audioEffect.setAttribute("src", "sounds/button-click.mp3")
+        audioEffect.play();
+    });
+    scissors.addEventListener('click', () => {
+        audioEffect.setAttribute("src", "sounds/button-click.mp3")
+        audioEffect.play();
+    });
+
+}
+//Winner sound
+function winnerEffect() {
+    const winnerEffect = document.createElement("audio")
+    if (computerPoints < userPoints) {
+        winnerEffect.setAttribute("src", "sounds/victory-sound.mp3")
+    } else {
+        winnerEffect.setAttribute("src", "sounds/lose-sound.mp3")
+    }
+    logsGame.appendChild(winnerEffect);
+    winnerEffect.play();
+
+}
 function game() {
-    computerPlay();
-    playRound();
-    logs();
-    reportScore();
-    if(computerPoints == 5 || userPoints == 5){
+    if (counter < 5) {
+        round();
+    }
+    if (counter <= 5) {
+        computerPlay();
+        playRound();
+        logs();
+        reportScore();
+    }
+    if (counter == 5) {
         winner();
+        winnerEffect();
     }
 }
 
 playerSelection();
+soundEffect();
 
